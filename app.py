a06de97e1b800ba8e3064c84ce8b80b7f520b3f3
kapilharode <31658291+kapilharode@users.noreply.github.com>
2020-08-31 12:13:32 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,54 @@
+import flask
+
+from flask import request
+
+app = flask.Flask(__name__)
+
+app.config["DEBUG"] = True
+
+
+from fask_cors import CORS
+
+CORS(app)
+
+
+# main index page route
+
+@app.route('/')
+
+def home():
+
+    return '<h1>API is working.. </h1>'
+
+
+
+
+@app.route('/predict',methods=['GET'])
+
+def predict():
+ 
+   from sklearn.externals import joblib
+ 
+   model = joblib.load('marriage_age_predict_model.ml')
+ 
+   predicted_age_of_marriage = model.predict([[(request.args['gender']),
+            
+                            (request.args['religion']),
+
+                            (request.args['caste']),
+
+                            (request.args['mother_tongue']),
+
+                            (request.args['country']),
+ 
+                           (request.args['height_cms']),
+
+                           ]])
+
+    return str(round(predicted_age_of_marriage[0],2))
+#return str(predicted_age_of_marriage)
+
+ 
+if __name__ == "__main__":
+ 
+   app.run(debug=True)
